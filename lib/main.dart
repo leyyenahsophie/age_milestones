715dc64d3ededa3d2b264ebfb4033bb36c551bf4
leyyenah <leyyenah@Leyyenahs-Air.wifi.gsu.edu>
2025-02-24 14:13:49 -0500

    Updated main.dart with age milestones feature

--- lib/main.dart
+++ lib/main.dart
@@ -7,16 +7,7 @@ import 'package:window_size/window_size.dart';
 void main() {
   setupWindow();
   runApp(
-    // Provide the model to all widgets within the app. We're using
-    // ChangeNotifierProvider because that's a simple way to rebuild
-    // widgets when a model changes. We could also just use
-    // Provider, but then we would have to listen to Counter ourselves.
-    //
-    // Read Provider's docs to learn about all the available providers.
     ChangeNotifierProvider(
-      // Initialize the model in the builder. That way, Provider
-      // can own Counter's lifecycle, making sure to call `dispose`
-      // when not needed anymore.
       create: (context) => Counter(),
       child: const MyApp(),
     ),
@@ -42,16 +33,19 @@ void setupWindow() {
   }
 }
 
-/// Simplest possible model, with just one field.
-///
-/// [ChangeNotifier] is a class in `flutter:foundation`. [Counter] does
-/// _not_ depend on Provider.
 class Counter with ChangeNotifier {
   int value = 0;
 
   void increment() {
     value += 1;
     notifyListeners();
+  }
+
+  void decrement() {
+    if (value > 0) {
+      value -= 1;
+      notifyListeners();
+    }
   }
 }
 
@@ -61,7 +55,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Age Milestone Counter',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         useMaterial3: true,
@@ -74,52 +68,69 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
 
+  String getMilestoneMessage(int age) {
+    if (age <= 12) {
+      return "You're a child!";
+    } else if (age <= 19) {
+      return "Teenager time!";
+    } else if (age <= 30) {
+      return "You're a young adult!";
+    } else if (age <= 50) {
+      return "Ew your an adult now!";
+    } else {
+      return "Golden years!";
+    }
+  }
+
+  Color getBackgroundColor(int age) {
+    if (age <= 12) {
+      return Colors.deepPurple;
+    } else if (age <= 19) {
+      return Colors.pinkAccent;
+    } else if (age <= 30) {
+      return Colors.blueAccent
+    } else if (age <= 50) {
+      return Colors.redAccent;
+    } else {
+      return Colors.grey;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    var counter = context.watch<Counter>();
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Flutter Demo Home Page'),
+        title: const Text('Age Milestone Counter'),
       ),
+      backgroundColor: getBackgroundColor(counter.value),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('You have pushed the button this many times:'),
-            // Consumer looks for an ancestor Provider widget
-            // and retrieves its model (Counter, in this case).
-            // Then it uses that model to build widgets, and will trigger
-            // rebuilds if the model is updated.
-            Consumer<Counter>(
-              builder: (context, counter, child) => Text(
-                '${counter.value}',
-                style: Theme.of(context).textTheme.headlineMedium,
-              ),
-            ),
+            Text(getMilestoneMessage(counter.value),
+                style: Theme.of(context).textTheme.headlineMedium),
+            const SizedBox(height: 20),
+            Text('${counter.value}',
+                style: Theme.of(context).textTheme.displayLarge),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // You can access your providers anywhere you have access
-          // to the context. One way is to use Provider.of<Counter>(context).
-          // The provider package also defines extension methods on the context
-          // itself. You can call context.watch<Counter>() in a build method
-          // of any widget to access the current state of Counter, and to ask
-          // Flutter to rebuild your widget anytime Counter changes.
-          //
-          // You can't use context.watch() outside build methods, because that
-          // often leads to subtle bugs. Instead, you should use
-          // context.read<Counter>(), which gets the current state
-          // but doesn't ask Flutter for future rebuilds.
-          //
-          // Since we're in a callback that will be called whenever the user
-          // taps the FloatingActionButton, we are not in the build method here.
-          // We should use context.read().
-          var counter = context.read<Counter>();
-          counter.increment();
-        },
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          FloatingActionButton(
+            onPressed: counter.decrement,
+            tooltip: 'Decrement',
+            child: const Icon(Icons.remove),
+          ),
+          FloatingActionButton(
+            onPressed: counter.increment,
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          ),
+        ],
       ),
     );
   }
